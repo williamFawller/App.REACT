f43cc56574072d66b05f966a8058eb30e3dbc832
williamFawller <95115492+williamFawller@users.noreply.github.com>
2021-11-27 11:13:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import './App.css';
+import {useEffect,useState} from 'react';
+const axios=require('axios').default;
+
+function App() {
+  const[options,setOptions]=useState([])
+  const [to,setTo]=useState("en");
+  const [from,setFrom]=useState("en");
+  const [input,setInput]=useState("");
+  const [output,setOutput]=useState('');
+
+  const translate = () =>{
+    
+    const params= new URLSearchParams();
+    params.append('q',input);
+    params.append('source',from);
+      params.append('target',to);
+        params.append('api_key','xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'); 
+    
+        axios.post('https://libretranslate.de/translate',params,{
+    q:input,
+    source:from,
+    target:to,
+    api_key:'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+  },{
+      headers:{
+      'accept':'application/json',
+    'Content-Type':'application/x-www-form-urlencoded',
+},
+}).then(res=>{
+  console.log(res.data)
+  setOutput(res.data.translatedText)
+  })
+};
+
+useEffect(() => {
+     axios.get('https://libretranslate.de/languages',
+     {headers:{accept:'application/json'}
+    })
+    .then((res)=>{
+      console.log(res.data);
+      setOptions(res.data);
+    });
+  }, []);
+
+  return (
+  <div className="App">
+    <div>
+      From ({from}):
+    <select onChange={e=>setFrom(e.target.value)}>
+      {options.map(opt=>(
+      <option key={opt.code} value={opt.code}>{opt.name} </option>))}
+      </select>
+      
+      To({to}):
+       <select onChange={e=>setTo(e.target.value)}>
+      {options.map(opt=>( 
+      <option key={opt.code}  value={opt.code}>{opt.name} </option>
+      ))}
+      </select>
+      </div>
+      <div>
+        <textarea clos="50" rows="8"  onInput={(e)=>setInput(e.target.value)}></textarea>
+        </div>
+        
+        <div>
+        <textarea clos="50" rows="8" value={output}></textarea>
+        </div>
+
+        <div>
+          <button onClick={e=>translate()}> Translate </button>
+          </div>
+  </div>
+  );
+}
+
+
+export default App;
